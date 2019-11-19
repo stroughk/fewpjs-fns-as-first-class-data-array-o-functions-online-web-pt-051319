da28a4a2f490402f38dbd27f491beb4f7aadb465
Kateryna Strough <katerynastrough@hotmail.com>
2019-11-19 23:42:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ var leashDog = function(dogName="Byron", dogBreed="poodle") {
   console.log(`Leash ${dogName} the ${dogBreed}`);
   return `Leash ${dogName} the ${dogBreed}`
 }
+
+var walkToPark = function(dogName="Byron", dogBreed="poodle") {
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  return `Walk to the park with ${dogName} the ${dogBreed}`
+}
+
+var throwFrisbee = function(dogName="Byron", dogBreed="poodle") {
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  return `Throw the frisbee for ${dogName} the ${dogBreed}`
+}
